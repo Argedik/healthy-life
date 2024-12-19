f2d63cd0bcaa2c91365ba42c802877e897b2bddc
Argedik <gedikas@hotmail.com>
2024-12-19 11:15:10 +0300

    Created file and folder macro

---
+++ frontend/web/src/screens/screens/test.rs
@@ -0,0 +1,11 @@
+﻿use yew::prelude::*;
+
+#[function_component(test)]
+pub fn test_component() -> Html {
+    let html_content = include_str!("test.html"); 
+    html! {
+        <div>
+            { Html::from_html_unchecked(html_content.into()) }
+        </div>
+    }
+}
